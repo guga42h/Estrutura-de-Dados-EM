727b9a83fafb843a351495449c1ae022c779b66e
guga42h <gustavoem1998@hotmail.com.br>
2018-11-07 01:26:33 -0200

    Primeiro commit

---
+++ EAD/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nd = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nd[0]);
+
+            int d = Convert.ToInt32(nd[1]);
+
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+            ;
+        }
+    }
+}
